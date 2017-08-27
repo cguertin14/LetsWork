3d258f0f858225281674ebea9c3496ea90b6d347
Unknown <charlesguertin@live.ca>
2017-08-27 16:27:10 -0400

    Bootstrap installé

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,13 @@
 const { mix } = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .sass('resources/assets/sass/app.scss', 'public/css');
 
 mix.styles([
-    'resources/assets/css/libs/bootstrap.css',
+    'resources/assets/css/bootstrap.css'
 ],'public/css/libs.css');
 
 mix.scripts([
-    'resources/assets/js/libs/jquery-3.2.1.js',
-    'resources/assets/js/libs/bootstrap.js'
+    'resources/assets/js/jquery-3.2.1.js',
+    'resources/assets/js/bootstrap.js',
 ],'public/js/libs.js');
