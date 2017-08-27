303fdc36b6c405fab81675c7f4eaa91ee48260fc
Unknown <ludovic.lachance@live.fr>
2017-08-27 16:23:36 -0400

    ok

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,10 +4,10 @@ mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 
 mix.styles([
-    'resources/assets/css/bootstrap.css'
+    'resources/assets/css/libs/bootstrap.css',
 ],'public/css/libs.css');
 
 mix.scripts([
-    'resources/assets/js/bootstrap.js',
-    'resources/assets/js/jquery-3.2.1.js',
+    'resources/assets/js/libs/jquery-3.2.1.js',
+    'resources/assets/js/libs/bootstrap.js'
 ],'public/js/libs.js');
