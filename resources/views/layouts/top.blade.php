9de7875c4bcb2a4a571b16f0f9c815c1b5b9f6ab
Unknown <ludovic.lachance@live.fr>
2017-09-01 21:51:57 -0400

    icon

--- resources/views/layouts/top.blade.php
+++ resources/views/layouts/top.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="{{asset('image/lw.png')}}" type="image/png">
 
         <title>LetsWork @yield('title')</title>
         <!--jQuery js-->
