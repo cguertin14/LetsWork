52153a8123108caa62d752e1b6fc40c741477b06
Unknown <charlesguertin@live.ca>
2017-09-11 23:29:23 -0400

    Homepage fix, Login,Register done,Forget psswd almost done

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,74 +13,67 @@
         <div class="middle">
             <div class="inner">
                 <div class="text-center">
-                    <img src="{{asset('image/LetsWw.png')}}" height="100px" width="auto" alt="">
+                    <a  href="/"><img src="{{asset('image/LetsWw.png')}}" height="100px" width="auto" alt=""></a>
                 </div>
                 <br>
                 <br>
-                <div class="container">
+                <div class="container" style="width: 50%;">
                     <div class="row">
                         <div class="col-md-8 col-md-offset-2">
                             <div class="panel panel-default">
-                                <div class="panel-heading">Register</div>
+                                <div class="panel-heading text-center">S'inscrire</div>
                                 <div class="panel-body">
                                     <form class="form-horizontal" method="POST" action="{{ route('register') }}">
                                         {{ csrf_field() }}
 
                                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                                            <div class="col-md-6">
-                                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                            <div class="col-md-12">
+                                                <input id="name" placeholder="Nom d'usager" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
                                                 @if ($errors->has('name'))
                                                     <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
+                                                        <strong>{{ $errors->first('name') }}</strong>
+                                                    </span>
                                                 @endif
                                             </div>
                                         </div>
 
                                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                                            <div class="col-md-6">
-                                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                            <div class="col-md-12">
+                                                <input id="email" type="email" placeholder="Adresse courriel" class="form-control" name="email" value="{{ old('email') }}" required>
 
                                                 @if ($errors->has('email'))
                                                     <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
+                                                        <strong>{{ $errors->first('email') }}</strong>
+                                                    </span>
                                                 @endif
                                             </div>
                                         </div>
 
                                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                                            <div class="col-md-6">
-                                                <input id="password" type="password" class="form-control" name="password" required>
+                                            <div class="col-md-12">
+                                                <input id="password" placeholder="Mot de passe" type="password" class="form-control" name="password" required>
 
                                                 @if ($errors->has('password'))
                                                     <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
+                                                        <strong>{{ $errors->first('password') }}</strong>
+                                                    </span>
                                                 @endif
                                             </div>
                                         </div>
 
                                         <div class="form-group">
-                                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                                            <div class="col-md-6">
-                                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                            <div class="col-md-12">
+                                                <input id="password-confirm" placeholder="Mot de passe de confirmation" type="password" class="form-control" name="password_confirmation" required>
                                             </div>
                                         </div>
 
                                         <div class="form-group">
-                                            <div class="col-md-6 col-md-offset-4">
-                                                <button type="submit" class="btn btn-primary">
-                                                    Register
+                                            <div class="col-md-12">
+                                                <button type="submit" class="btn purplebtn">
+                                                    Soumettre
                                                 </button>
+                                                <a href="/login" class="btn btn-link pull-right">Je possède un compte</a>
                                             </div>
                                         </div>
                                     </form>
