7b9843a43a99eaf06370d7de84b2537f39ac98bb
Unknown <ludovic.lachance@live.fr>
2017-09-12 19:30:08 -0400

    a bit more 25min

--- resources/views/company/create.blade.php
+++ resources/views/company/create.blade.php
@@ -6,7 +6,12 @@
 
 @section('styles')
     <style>
-
+        body{
+            background-color: #474747;
+        }
+        select{
+            height:36px !important;
+        }
     </style>
 @endsection
 
@@ -20,7 +25,13 @@
                             <div class="panel panel-default">
                                 <div class="panel-heading text-center">Creation d'entreprise</div>
                                 <div class="panel-body">
-                                    <form action="">
+                                    <form class="form-group" action="">
+                                        <div class="row">
+                                            <div class="col">
+                                                <label for="logofile">Le logo de votre entreprise</label>
+                                                <input id="logofile" name="logo" type="file" class="form-control-file">
+                                            </div>
+                                        </div>
                                         <div class="row">
                                             <div class="col">
                                                 <input name="telephone" type="text" class="form-control"
@@ -48,7 +59,8 @@
                                             </div>
                                             <div class="col">
                                                 <div class="form-group">
-                                                    <select class="form-control" name="pays" id="selecteurpays">
+                                                    <select class="form-control custom-select" name="pays" id="selecteurpays">
+                                                        <option selected>Choisissez un pays</option>
                                                         @foreach($country_list as $pays)
                                                             <option value="{{$pays}}">{{$pays}}</option>
                                                         @endforeach
