484dce0846c54d0cf9eee312ab4185707f446d57
Unknown <charlesguertin@live.ca>
2017-09-15 00:22:44 -0400

    Auto stash before merge of "dev" and "origin/dev"

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
     <div class="col-md-4" style="height: 100%;">
-        <h1 class="text-center">Mon profil</h1>
-        <div class="text-center" style="width:100%; height:200px">
+        <h1>Mon profil</h1>
+        <div style="width:100%; height:100%">
             <img src="http://www.garcard.com/images/garcard_symbol.png" style="border-radius: 50%">
         </div>
         <p class="text-center">{{ $user->name }}</p>
