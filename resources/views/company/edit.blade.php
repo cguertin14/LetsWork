9110946a8ab2359cc3931b6ca20a7ab77c18b9c3
Unknown <ludovic.lachance@live.fr>
2017-09-17 19:48:36 -0400

    reste show

---
+++ resources/views/company/edit.blade.php
@@ -0,0 +1,81 @@
+@php
+    include resource_path("arrays/country.php");
+@endphp
+
+@extends('layouts.master')
+
+@section('styles')
+    <style>
+        body {
+            background-color: #5d5d5d;
+        }
+
+        select {
+            height: 36px !important;
+        }
+
+        label, input {
+            color: white;
+        }
+    </style>
+@endsection
+
+@section('content')
+    <div class="col-md-12">
+        <h1 class="h1" style="color: white">Création d'entreprise</h1>
+        {{Form::open(['action' => ['CompanyController@update', $data["id"]]])}}
+        {{ method_field('PATCH') }}
+        <div class="row">
+            <div class="col-lg-6">
+                {{Form::text("name",$data["name"], array('class' => 'form-control',"placeholder"=>"Nom d'entreprise"))}}
+            </div>
+            <div class="col-lg-6">
+                {{Form::label('logofile', 'Le logo de votre entreprise')}}
+                {{Form::file('logo',array("id"=>"logofile"))}}
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-lg-6">
+                {{Form::text("telephone",$data["telephone"], array('class' => 'form-control',"placeholder"=>"Telephone"))}}
+            </div>
+            <div class="col-lg-6">
+                {{Form::text("email",$data["email"], array('class' => 'form-control',"placeholder"=>"Email"))}}
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-lg-6">
+                {{Form::text("ville",$data["ville"], array('class' => 'form-control',"placeholder"=>"Ville"))}}
+            </div>
+            <div class="col-lg-6">
+                {{Form::text("adresse",$data["adresse"], array('class' => 'form-control',"placeholder"=>"Adresse"))}}
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-lg-6">
+                {{Form::text("zipcode",$data["zipcode"], array('class' => 'form-control',"placeholder"=>"Code postal"))}}
+            </div>
+            <div class="col-lg-6">
+                {{Form::select('pays', $country_list, array_search($data["pays"],$country_list), array('class' => 'form-control custom-select'))}}
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-lg-6">
+                {{Form::select('company_type_id', $companyTypes , array_search($data["company_type_id"],$companyTypes), array('class' => 'form-control custom-select'))}}
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-lg-12">
+                {{Form::textarea("description", $data["description"] ,array('class' => 'form-control',"placeholder"=>"Description","rows"=>3))}}
+            </div>
+        </div>
+        <br>
+        {{Form::submit('Editer',array('class' => 'btn btn-primary purplebtn'))}}
+        {{Form::close()}}
+    </div>
+    </div>
+@endsection
--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -9,20 +9,22 @@
 @section('content')
     <div class="col-md-12">
         <div class="row">
-            <div class="col-md">
+            <div class="col-md-12">
                 <h1 style="text-align: center" class="h1">{{$data['name']}}</h1>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6">
-                <img style="width: 40%;height: auto" alt="logo">
+            <div class="col-md-12">
+                <img src="https://www.w3schools.com/css/trolltunga.jpg" style="width: 100%;height: auto ;background-color: black" alt="logo">
             </div>
+        </div>
+        <div class="row">
             <div class="col-md-6">
                 <div class="row">
                     <h4 class="h4">{{$data['adresse']}}</h4>
                 </div>
                 <div class="row">
-                <h4 class="h4">{{$data['ville']}}</h4>
+                    <h4 class="h4">{{$data['ville']}}</h4>
                 </div>
             </div>
         </div>
