5188da1dae8643a10c1ec8bb56732b754a73ddce
Unknown <ludovic.lachance@live.fr>
2017-09-17 21:11:59 -0400

    tout marche sauf menu gauche

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,7 @@
 const { mix } = require('laravel-mix');
 
-mix.js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css');
-
 mix.styles([
     'resources/assets/css/tether.css',
-    'resources/assets/css/bootstrap.css',
     'resources/assets/css/homepage.css',
     'resources/assets/css/tools.css',
     'resources/assets/css/forgetpassword.css'
@@ -14,5 +10,8 @@ mix.styles([
 mix.scripts([
     'resources/assets/js/jquery-3.2.1.js',
     'resources/assets/js/tether.js',
-    'resources/assets/js/bootstrap.js',
+    'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
 ],'public/js/libs.js');
+
+mix.js('resources/assets/js/app.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css');
