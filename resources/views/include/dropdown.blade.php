5358310b068547f5707c422197b6ce461e518bd8
Unknown <charlesguertin@live.ca>
2017-10-17 13:39:21 -0400

    Fix menu

--- resources/views/include/dropdown.blade.php
+++ resources/views/include/dropdown.blade.php
@@ -13,7 +13,11 @@
     }
 
     dropdown("#dropdown1","#dropdown1Title",100,"#img1");
-    dropdown("#dropdown2","#dropdown2Title",50 * {{count(\Illuminate\Support\Facades\Auth::user()->companies)}},"#img2");
+    @php($height = 0)
+    @foreach(\Illuminate\Support\Facades\Auth::user()->companies as $company)
+        @php($height+=50)
+    @endforeach
+    dropdown("#dropdown2","#dropdown2Title",{{$height}},"#img2");
     dropdown("#dropdown3","#dropdown3Title",250,"#img3");
     dropdown("#dropdown4","#dropdown4Title",100,"#img4");
     dropdown("#dropdown5","#dropdown5Title",100,"#img5");
