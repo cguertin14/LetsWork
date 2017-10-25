1b78c9a251b19b175d8065fc34b7f9e27bfa2faf
Unknown <ludovic.lachance@live.fr>
2017-10-25 02:50:27 -0400

    creation du protocole de connexion socket.io pour duo

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -8,9 +8,11 @@
 
 @section('content')
     <div id="chat">
-        <ul>
-            <li v-for="mess in messages">@{{mess}}</li>
+        <div id="chatbox" style="overflow: scroll;height: 20em;">
+        <ul class="list-group">
+            <li class="" v-for="mess in messages">@{{mess}}</li>
         </ul>
+        </div>
         <input type="text" v-model="message">
         <button v-on:click="send">Envoyer</button>
     </div>
@@ -23,18 +25,30 @@
             el:"#chat",
             methods:{
                 send: function (e) {
-                    socket.emit("chat.message", this.message);
+                    socket.emit("chat.message",{
+                        user:this.currentuser,
+                        message:this.message
+                    });
                     e.preventDefault();
+                },
+                connection:function () {
+                    socket.emit("user.connection",{
+                        user:this.currentuser
+                    });
                 }
             },
             data:{
-                currentuser:{{Auth}}
+                currentuser:'@if(!\Illuminate\Support\Facades\Auth::guest()){{\Illuminate\Support\Facades\Auth::user()->email}}@else Anon @endif',
                 message:'',
                 messages:[]
             },
             mounted:function () {
-                socket.on('chat.message',function (message) {
-                    this.messages.push(message);
+                socket.on('chat.message',function (data) {
+                    this.messages.push(data.user+"> "+data.message);
+                    $("#chatbox").scrollTop(100000000);
+                }.bind(this));
+                socket.on(this.currentuser+'.askedroomjoin',function (data) {
+                    alert(data.room);
                 }.bind(this));
             }
         });
--- socket.js
+++ socket.js
@@ -4,16 +4,61 @@ var io = require('socket.io')(http);
 var Redis = require('ioredis');
 var redis = new Redis();
 
-http.listen(3000, function(){
+function returnHash() {
+    var abc = "abcdefghijklmnopqrstuvwxyz1234567890".split("");
+    var token = "";
+    for (var i = 0; i < 128; i++) {
+        token += abc[Math.floor(Math.random() * abc.length)];
+    }
+    return token; //Will return a 32 bit "hash"
+}
+
+http.listen(3000, function () {
     console.log('Listening on Port 3000');
 });
 
-var users=new Map();
+var users = [];
+var chatrooms = new Map();
 
-io.on('connection',function (socket) {
+function echoback(data) {
+    io.emit('chat.message', data);
+}
 
-	socket.on('chat.message',function (message) {
-		console.log(message);
-		io.emit('chat.message',message);
-	});
+function connection(data) {
+    if (users.contains(data.user)) {
+        io.emit(data.user, {result: false});
+    }
+    else {
+        users.push(data.user);
+        io.emit(data.user, {result: true});
+    }
+}
+
+function duochatconnect(data) {
+    if (!users.has(data.user) || !users.has(data.requesteduser)) {
+        io.emit('user.request.duochat.' + data.user, {result: false});
+    }
+    else {
+        var roomname = 'user.chatroom.duo.' + data.user + '.' + data.requesteduser;
+        var token=returnHash();
+        io.emit('user.request.duochat.' + data.user, {result: true, chatroom: room, token: token});
+        io.emit(data.requesteduser+'.askedroomjoin',roomname);
+        socket.on(roomname, function (data) {
+            io.emit(roomname, data);
+        });
+        chatrooms.set(room, token);
+    }
+}
+
+function duochatunconnect(data) {
+    if (chatrooms.get(data.roomname) === data.token) {
+        socket.removeAllListeners(data.roomname);
+    }
+}
+
+io.on('connection', function (socket) {
+    socket.on('chat.message', echoback);
+    socket.on('user.connection', connection);
+    socket.on('user.request.duochat.connect', duochatconnect);
+    socket.on('user.request.duochat.unconnect', duochatunconnect);
 });
