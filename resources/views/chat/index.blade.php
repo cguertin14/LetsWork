f3b862003f572d3cb7c0bf4950f5eea211a75c6d
Unknown <ludovic.lachance@live.fr>
2017-11-28 02:54:14 -0500

    edit

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -2,66 +2,66 @@
 
 @section('styles')
     <style>
-    body{
-        background-color:#5d5d5d;
-    }
+        body {
+            background-color: #5d5d5d;
+        }
 
-.parent{
-    height: 100%;
-    width: 100%;
-    overflow: hidden;
-    position: relative;
-}
+        .parent {
+            height: 100%;
+            width: 100%;
+            overflow: hidden;
+            position: relative;
+        }
 
-.child{
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    overflow-y: scroll;
-    margin-right: -100px;
-    padding-right: 100px;
-}
+        .child {
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            overflow-y: scroll;
+            margin-right: -100px;
+            padding-right: 100px;
+        }
 
-.boxsize{
-    width: 75%;
-    height: auto;
-    margin-bottom: .2em;
-    padding-right: .3em;
-    padding-left:  .3em;
-    border-radius: .7em;
-}
+        .boxsize {
+            width: 75%;
+            height: auto;
+            margin-bottom: .2em;
+            padding-right: .3em;
+            padding-left: .3em;
+            border-radius: .7em;
+        }
 
-.text{
-    vertical-align: middle;
-    padding: .3em;
-    margin: 0px;
-}
+        .text {
+            vertical-align: middle;
+            padding: .3em;
+            margin: 0px;
+        }
 
-.username{
-    background-color: transparent !important;
-}
+        .username {
+            background-color: transparent !important;
+        }
 
-.currentuser {
-    text-align: right;
-    color: white;
-    background-color: #552AD6;
-}
+        .currentuser {
+            text-align: right;
+            color: white;
+            background-color: #552AD6;
+        }
 
-.otheruser{
-    text-align: left;
-    color: white;
-    background-color: #474747;
-}
+        .otheruser {
+            text-align: left;
+            color: white;
+            background-color: #474747;
+        }
 
-.item{
-    cursor:pointer;
-}
-.red
-{
-    background-color: red;
-}
+        .item {
+            cursor: pointer;
+        }
+
+        .red {
+            background-color: red;
+        }
 
     </style>
 @endsection
@@ -72,7 +72,9 @@
             <h4 style="color: white; text-align: center;">Conversations</h4>
             <div class="parent" style="height: 100%;">
                 <div class="list-group child">
-                    <div class="list-group-item item" v-bind:class="seen(room)" v-for="room in Object.keys(rooms)" v-bind:data-room="room" v-on:click="setroom(room)">@{{room}}</div>
+                    <div class="list-group-item item" v-bind:class="seen(room)" v-for="room in Object.keys(rooms)"
+                         v-bind:data-room="room" v-on:click="setroom(room)">@{{room}}
+                    </div>
                 </div>
             </div>
         </div>
@@ -80,21 +82,24 @@
             <div class="parent" style="height: 100%;">
                 <div id="chatbox" class="list-group child row">
                     <div class="col-md-12" v-for="mess in currentmessages" v-bind:data-user="mess.user.name">
-                        <div v-bind:class="iscurrentuser(mess.user)" class="username"> @{{mess.user.name}} </div>
+                        <div v-bind:class="iscurrentuser(mess.user)" class="username"> @{{mess.user.name}}</div>
                         <div v-bind:class="iscurrentuser(mess.user)">
                             <p class="text">@{{mess.message}}</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <input class="form-control" placeholder="Envoyer un message..." style="width: 100%" type="text" v-model="message" v-on:keydown.enter="send">
-<!-- <button class="" v-on:click="send">Envoyer</button> -->
+            <input class="form-control" placeholder="Envoyer un message..." style="width: 100%" type="text"
+                   v-model="message" v-on:keydown.enter="send">
+            <!-- <button class="" v-on:click="send">Envoyer</button> -->
         </div>
         <div class="col-md-3" style="height: 100%;">
             <h4 style="color: white; text-align: center;">Utilisateurs Connectés</h4>
             <div class="parent" style="height: 100%;">
                 <div class="list-group child">
-                    <div class="list-group-item item" v-for="user in allotherusers()" v-bind:data-user="user.email" v-on:click="cchatroom(user)">@{{user.name}}</div>
+                    <div class="list-group-item item" v-for="user in allotherusers()" v-bind:data-user="user.email"
+                         v-on:click="cchatroom(user)">@{{user.name}}
+                    </div>
                 </div>
             </div>
         </div>
@@ -109,16 +114,14 @@
             methods: {
                 send: function (e) {
                     if (this.message != '') {
-                        if(this.currentroom === 'Entreprise')
-                        {
+                        if (this.currentroom === 'Entreprise') {
                             socket.emit("globalchat", {
                                 user: this.currentuser,
                                 message: this.message
                             });
                         }
-                        else
-                        {
-                            var message={
+                        else {
+                            var message = {
                                 hash: this.rooms[this.currentroom].hash,
                                 message: this.message,
                                 sender: this.currentuser,
@@ -136,121 +139,155 @@
                         data: null
                     });
                 },
-                allotherusers:function () {
-                    var cuser=this.currentuser;
-                    return this.allusersonline.filter(function(x) { return x.email !== cuser.email; });
+                allotherusers: function () {
+                    var cuser = this.currentuser;
+                    return this.allusersonline.filter(function (x) {
+                        return x.email !== cuser.email;
+                    });
                 },
                 iscurrentuser: function (user) {
-                    return this.currentuser.email===user.email? "boxsize currentuser pull-right": "boxsize otheruser pull-left";
+                    return this.currentuser.email === user.email ? "boxsize currentuser pull-right" : "boxsize otheruser pull-left";
                 },
-                cchatroom:function (user){
-                    if(this.rooms[user.name]==null) {
+                cchatroom: function (user) {
+                    if (this.rooms[user.name] == null) {
                         socket.emit("roomchat.create", {
                             sender: this.currentuser,
                             receivers: [user]
                         });
                     }
-                    else
-                    {
-                        this.currentroom=user.name;
+                    else {
+                        this.currentroom = user.name;
                     }
                 },
-                setroom:function (room) {
-                    $.when(this.currentroom=room).then(function () {
+                setroom: function (room) {
+                    $.when(this.currentroom = room).then(function () {
                         $("#chatbox").scrollTop($("#chatbox")[0].scrollHeight);
                     });
-                    this.rooms[this.currentroom]['seen']=true;
+                    this.rooms[this.currentroom]['seen'] = true;
                 },
-                savemessage:function (message) {
-                   // $.post('/test1', { message: message, _token: {{csrf_token()}} });
-                   $.ajax({
+                savemessage: function (message) {
+                    $.ajax({
                         method: 'POST',
                         url: '/savemessages',
-                        data: { message: message, _token: '{{csrf_token()}}' }
-                   });
+                        data: {message: message, _token: '{{csrf_token()}}'}
+                    });
                 },
-                seen:function (room) {
-                   return this.rooms[room].seen?"":"red";
+                seen: function (room) {
+                    return this.rooms[room].seen ? "" : "red";
                 },
-                loadlastmessages:function () {
-
+                loadlastmessages: function () {
+                    $.ajax({
+                        method: 'GET',
+                        url: '/lastmessages',
+                        success:function (data) {
+                            console.log(data);
+                            data.forEach(function (e) {
+                                if(e.sender_id==this.myid)
+                                {
+                                    app.rooms[e.receiver_name] = {};
+                                    app.rooms[e.receiver_name]['messages'] = [];
+                                    app.rooms[e.receiver_name]['hash'] = '';
+                                    app.rooms[e.receiver_name]['user'] = {email:e.receiver_email,name:e.receiver_name};
+                                    app.rooms[e.receiver_name]['seen'] = true;
+                                    app.rooms[e.receiver_name]['messages'].push({user: {email:e.receiver_email,name:e.receiver_name}, message: e.content});
+                                }
+                                if(e.receiver_id==this.myid)
+                                {
+                                    app.rooms[e.sender_name] = {};
+                                    app.rooms[e.sender_name]['messages'] = [];
+                                    app.rooms[e.sender_name]['hash'] = '';
+                                    app.rooms[e.sender_name]['user'] = {email:e.sender_email,name:e.sender_name};
+                                    app.rooms[e.sender_name]['seen'] = true;
+                                    app.rooms[e.sender_name]['messages'].push({user: {email:e.sender_email,name:e.sender_name}, message: e.content});
+                                }
+                            });
+                        }
+                    });
                 }
             },
             computed: {
-                currentmessages:function () {
+                currentmessages: function () {
                     return this.rooms[this.currentroom].messages;
                 }
             },
             data: {
-                currentuser: @if(!\Illuminate\Support\Facades\Auth::guest()){email:'{{\Illuminate\Support\Facades\Auth::user()->email}}',name:'{{\Illuminate\Support\Facades\Auth::user()->name}}'}
+                currentuser: @if(!\Illuminate\Support\Facades\Auth::guest()){
+                    email: '{{\Illuminate\Support\Facades\Auth::user()->email}}',
+                    name: '{{\Illuminate\Support\Facades\Auth::user()->name}}'
+                }
                 @else 'Anon' @endif,
                 message: '',
-                rooms:{Entreprise:{messages:[],seen:true}},
-                currentroom:'Entreprise',
+                rooms: {Entreprise: {messages: [], seen: true}},
+                currentroom: 'Entreprise',
                 messages: [],
+                myid:{{\Illuminate\Support\Facades\Auth::id()}},
                 allusersonline: [],
                 auth: @if(!\Illuminate\Support\Facades\Auth::guest()) {{'true'}} @else {{'false'}} @endif
             },
             mounted: function () {
-                var app=this;
+                var app = this;
 
                 socket.on('globalchat.message', function (data) {
-                    $.when(app.rooms['Entreprise'].messages.push({user:data.user, message:data.message}))
-                        .then(function(){
-                    $("#chatbox").scrollTop($("#chatbox")[0].scrollHeight)
+                    $.when(app.rooms['Entreprise'].messages.push({user: data.user, message: data.message}))
+                        .then(function () {
+                            $("#chatbox").scrollTop($("#chatbox")[0].scrollHeight)
                         });
-                        if('Entreprise'!=app.currentroom)
-                        app.rooms['Entreprise']['seen']=false;
+                    if ('Entreprise' != app.currentroom)
+                        app.rooms['Entreprise']['seen'] = false;
                 }.bind(this));
 
                 socket.on('globalchat.users', function (data) {
                     this.allusersonline = $.parseJSON(data);
                 }.bind(this));
 
-                socket.on('roomchat.invite.'+app.currentuser.email, function (data) {
+                socket.on('roomchat.invite.' + app.currentuser.email, function (data) {
                     socket.emit("roomchat.confirm", {
                         confirm: true,
-                        sender:app.currentuser,
-                        hash:data.hash
+                        sender: app.currentuser,
+                        hash: data.hash
                     });
-                    app.rooms[data.sender.name]={};
-                    app.rooms[data.sender.name]['messages']=[];
-                    app.rooms[data.sender.name]['hash']=data.hash;
-                    app.rooms[data.sender.name]['user']=data.sender;
-                    app.rooms[data.sender.name]['seen']=true;
-                    socket.on('roomchat.'+data.hash, function (data2) {
-                        $.when(app.rooms[data.sender.name].messages.push({user:data2.sender, message:data2.message}))
-                            .then(function(){
+                    app.rooms[data.sender.name] = {};
+                    app.rooms[data.sender.name]['messages'] = [];
+                    app.rooms[data.sender.name]['hash'] = data.hash;
+                    app.rooms[data.sender.name]['user'] = data.sender;
+                    app.rooms[data.sender.name]['seen'] = true;
+                    socket.on('roomchat.' + data.hash, function (data2) {
+                        $.when(app.rooms[data.sender.name].messages.push({user: data2.sender, message: data2.message}))
+                            .then(function () {
                                 $.when(app.$forceUpdate()).then(function () {
                                     $("#chatbox").scrollTop($("#chatbox")[0].scrollHeight);
                                 });
                             });
-                        if(data.sender.name!=app.currentroom)
-                        app.rooms[data.sender.name]['seen']=false;
+                        if (data.sender.name != app.currentroom)
+                            app.rooms[data.sender.name]['seen'] = false;
                     }.bind(app));
-                    app.currentroom=data.sender.name;
+                    app.currentroom = data.sender.name;
                 }.bind(this));
 
-                socket.on('roomchat.'+app.currentuser.email, function (data) {
-                    app.rooms[data.receiver.name]={};
-                    app.rooms[data.receiver.name]['messages']=[];
-                    app.rooms[data.receiver.name]['hash']=data.hash;
-                    app.rooms[data.receiver.name]['user']=data.receiver;
-                    app.rooms[data.receiver.name]['seen']=true;
-                    socket.on('roomchat.'+data.hash, function (data2) {
-                        $.when(app.rooms[data.receiver.name]['messages'].push({user:data2.sender, message:data2.message}))
-                            .then(function(){
+                socket.on('roomchat.' + app.currentuser.email, function (data) {
+                    app.rooms[data.receiver.name] = {};
+                    app.rooms[data.receiver.name]['messages'] = [];
+                    app.rooms[data.receiver.name]['hash'] = data.hash;
+                    app.rooms[data.receiver.name]['user'] = data.receiver;
+                    app.rooms[data.receiver.name]['seen'] = true;
+                    socket.on('roomchat.' + data.hash, function (data2) {
+                        $.when(app.rooms[data.receiver.name]['messages'].push({
+                            user: data2.sender,
+                            message: data2.message
+                        }))
+                            .then(function () {
                                 $.when(app.$forceUpdate()).then(function () {
                                     $("#chatbox").scrollTop($("#chatbox")[0].scrollHeight);
                                 });
-                        });
-                        if(data.receiver.name!=app.currentroom)
-                        app.rooms[data.receiver.name]['seen']=false;
+                            });
+                        if (data.receiver.name != app.currentroom)
+                            app.rooms[data.receiver.name]['seen'] = false;
                     }.bind(app));
-                    app.currentroom=data.receiver.name;
+                    app.currentroom = data.receiver.name;
                 }.bind(this));
 
                 this.getuserlist();
+                this.loadlastmessages();
             }
         });
     </script>
--- routes/web.php
+++ routes/web.php
@@ -35,94 +35,99 @@ Route::resource('company', 'CompanyController');
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
-	/* Profile Routes */
-	Route::get('/profile/{slug}', 'ProfileController@view')->name('profile.view');
-	Route::get('/profile/{slug}/edit', 'ProfileController@edit')->name('profile.edit');
-	Route::patch('/profile/{slug}/update', 'ProfileController@update')->name('profile.update');
-	Route::patch('/profile/uploadphoto', 'ProfileController@uploadphoto')->name('profile.uploadphoto');
-	Route::get('/profilephoto', 'ProfileController@photo')->name('profile.photo');
-	Route::delete('/profile/{slug}/delete', 'ProfileController@deleteuser')->name('profile.delete');
+    /* Profile Routes */
+    Route::get('/profile/{slug}', 'ProfileController@view')->name('profile.view');
+    Route::get('/profile/{slug}/edit', 'ProfileController@edit')->name('profile.edit');
+    Route::patch('/profile/{slug}/update', 'ProfileController@update')->name('profile.update');
+    Route::patch('/profile/uploadphoto', 'ProfileController@uploadphoto')->name('profile.uploadphoto');
+    Route::get('/profilephoto', 'ProfileController@photo')->name('profile.photo');
+    Route::delete('/profile/{slug}/delete', 'ProfileController@deleteuser')->name('profile.delete');
 
-	/* Company Routes */
-	Route::post('/company/{slug}/select', 'CompanyController@select')->name('company.select');
-	Route::post('/company/uploadphoto', 'CompanyController@uploadphoto')->name('company.uploadphoto');
+    /* Company Routes */
+    Route::post('/company/{slug}/select', 'CompanyController@select')->name('company.select');
+    Route::post('/company/uploadphoto', 'CompanyController@uploadphoto')->name('company.uploadphoto');
 
-	/* Absence Routes */
-	Route::resource('absence', 'AbsenceController');
+    /* Absence Routes */
+    Route::resource('absence', 'AbsenceController');
 
-	/* Special Roles Routes */
-	Route::resource('specialrole', 'SpecialRoleController');
+    /* Special Roles Routes */
+    Route::resource('specialrole', 'SpecialRoleController');
 
-	/* Dispo Routes */
-	Route::resource('dispo', 'DispoController');
+    /* Dispo Routes */
+    Route::resource('dispo', 'DispoController');
 
-	/* Skills Routes */
-	Route::resource('skill', 'SkillController');
+    /* Skills Routes */
+    Route::resource('skill', 'SkillController');
 
-	/* Cv Routes */
-	Route::get('/cv/create', 'CvController@create')->name('cv.create');
-	Route::get('/cv', 'CvController@getAuthCv')->name('cv.get');
-	Route::post('/cv/store', 'CvController@store')->name('cv.store');
+    /* Cv Routes */
+    Route::get('/cv/create', 'CvController@create')->name('cv.create');
+    Route::get('/cv', 'CvController@getAuthCv')->name('cv.get');
+    Route::post('/cv/store', 'CvController@store')->name('cv.store');
 
-	/* JobOffer Routes (suite...) */
-	Route::post('/joboffer/{slug}/apply', 'JobOfferController@apply')->name('joboffer.apply');
+    /* JobOffer Routes (suite...) */
+    Route::post('/joboffer/{slug}/apply', 'JobOfferController@apply')->name('joboffer.apply');
 
-	/* JobOfferUser Routes */
-	Route::get('/jobofferuser', 'JobOfferUserController@index')->name('jobofferuser.index');
-	Route::get('/jobofferuser/{id}', 'JobOfferUserController@show')->name('jobofferuser.show');
-	Route::post('/jobofferuser/{id}/accept', 'JobOfferUserController@accept')->name('jobofferuser.accept'); //->middleware('manager');
-	Route::post('/jobofferuser/{id}/interview', 'JobOfferUserController@interview')->name('jobofferuser.interview'); //->middleware('manager');
-	Route::delete('/jobofferuser/{id}/refuse', 'JobOfferUserController@refuse')->name('jobofferuser.refuse');
+    /* JobOfferUser Routes */
+    Route::get('/jobofferuser', 'JobOfferUserController@index')->name('jobofferuser.index');
+    Route::get('/jobofferuser/{id}', 'JobOfferUserController@show')->name('jobofferuser.show');
+    Route::post('/jobofferuser/{id}/accept', 'JobOfferUserController@accept')->name('jobofferuser.accept'); //->middleware('manager');
+    Route::post('/jobofferuser/{id}/interview', 'JobOfferUserController@interview')->name('jobofferuser.interview'); //->middleware('manager');
+    Route::delete('/jobofferuser/{id}/refuse', 'JobOfferUserController@refuse')->name('jobofferuser.refuse');
 
-	/* Schedule Routes */
-	Route::get('/schedule/thisweek', 'ScheduleController@thisweek')->name('schedule.thisweek');
-	Route::get('/schedule/week/{datebegin}', 'ScheduleController@week')->name('schedule.week');
-	Route::get('/schedule/scheduleelement', 'ScheduleController@createelement')->name('schedule.createelement');
-	Route::post('/schedule/scheduleelement', 'ScheduleController@storeelement')->name('schedule.storeelement');
-	Route::get('/schedule/editing', 'ScheduleController@editing')->name('schedule.editing'); //->middleware('manager');
-	Route::get('/schedule/employees/{specialrole}', 'ScheduleController@getEmployees')->name('schedule.employees');
-	Route::resource('/schedule', 'ScheduleController');
+    /* Schedule Routes */
+    Route::get('/schedule/thisweek', 'ScheduleController@thisweek')->name('schedule.thisweek');
+    Route::get('/schedule/week/{datebegin}', 'ScheduleController@week')->name('schedule.week');
+    Route::get('/schedule/scheduleelement', 'ScheduleController@createelement')->name('schedule.createelement');
+    Route::post('/schedule/scheduleelement', 'ScheduleController@storeelement')->name('schedule.storeelement');
+    Route::get('/schedule/editing', 'ScheduleController@editing')->name('schedule.editing'); //->middleware('manager');
+    Route::get('/schedule/employees/{specialrole}', 'ScheduleController@getEmployees')->name('schedule.employees');
+    Route::resource('/schedule', 'ScheduleController');
 
-	/* Punch Routes */
-	Route::post('/punch', 'PunchController@add');
-	Route::get('/punches', 'PunchController@index')->name('punch');
-	Route::get('/punches/lastweek', 'PunchController@lastweek');
-	Route::get('/punches/lastmouth', 'PunchController@lastmouth');
-	Route::get('/punches/lastyear', 'PunchController@lastyear');
+    /* Punch Routes */
+    Route::post('/punch', 'PunchController@add');
+    Route::get('/punches', 'PunchController@index')->name('punch');
+    Route::get('/punches/lastweek', 'PunchController@lastweek');
+    Route::get('/punches/lastmouth', 'PunchController@lastmouth');
+    Route::get('/punches/lastyear', 'PunchController@lastyear');
 
-	/* Other Routes */
-	Route::get('/isauthmanager', 'OtherController@userIsManager');
+    /* Other Routes */
+    Route::get('/isauthmanager', 'OtherController@userIsManager');
 });
 
 Route::get('/fire', function () {
-	// this fires the event
-	event(new App\Events\ChatEvent());
-	return "event fired";
+    // this fires the event
+    event(new App\Events\ChatEvent());
+    return "event fired";
 });
 
 Route::get('/chat', function () {
-	// this checks for the event
-	return view('chat.index');
+    // this checks for the event
+    return view('chat.index');
 })->name('chat');
 
 Route::get('/test', function () {
-	$json = [];
-	try {
-		$redis = new \Predis\Client('localhost:6379');
-		foreach (\App\Session::connectedUsers() as $session) {
-			array_push($json, $session->user->email);
-		}
-		$redis->set('OnlineUsers', \GuzzleHttp\json_encode($json));
-	} catch (Exception $e) {
-		die($e->getMessage());
-	}
-	return \GuzzleHttp\json_encode($json);
+    $json = [];
+    try {
+        $redis = new \Predis\Client('localhost:6379');
+        foreach (\App\Session::connectedUsers() as $session) {
+            array_push($json, $session->user->email);
+        }
+        $redis->set('OnlineUsers', \GuzzleHttp\json_encode($json));
+    } catch (Exception $e) {
+        die($e->getMessage());
+    }
+    return \GuzzleHttp\json_encode($json);
 });
 
 Route::post('/savemessages', function (Request $request) {
-	\App\Message::create([
-		'sender_id' => \App\User::where('email', '=', $request->input('message.sender.email'))->first()->id,
-		'receiver_id' => \App\User::where('email', '=', $request->input('message.receiver.email'))->first()->id,
-		'content' => $request->input('message.message'),
-	]);
+    \App\Message::create([
+        'sender_id' => \App\User::where('email', '=', $request->input('message.sender.email'))->first()->id,
+        'receiver_id' => \App\User::where('email', '=', $request->input('message.receiver.email'))->first()->id,
+        'content' => $request->input('message.message'),
+    ]);
+});
+
+Route::get('/lastmessages', function () {
+    $allm = \App\Message::where('sender_id', '=', Auth::id())->orWhere('receiver_id', '=', Auth::id())->with(['sender', 'receiver'])->get();
+    return  $allm->toJson();
 });
