f54159ca67b0d83f6a2736f224fb1c5ab104825b
Unknown <charlesguertin@live.ca>
2017-12-27 16:28:43 -0500

    Page 503

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,15 +1,127 @@
 @extends('layouts.top')
 
+@section('styles')
+    <style>
+        img {
+            max-width:100%;
+        }
+        /*end reset*/
+        body{
+            background:url(../image/404.png);
+            overflow-y: hidden;
+        }
+        .wrap {
+            width:1000px;
+            margin:0 auto;
+        }
+        .main{
+            text-align:center;
+            background: rgba(255, 255, 255, 0.04);
+            color:#FFF;
+            font-weight:bold;
+            margin-top:160px;
+            border:1px solid rgba(102, 102, 102, 0.31);
+            -webkit-border-radius:5px;
+            -moz-border-radius:5px;
+            border-radius:5px;
+        }
+        .main h3{
+            font-size:16px;
+            text-align:left;
+            padding:10px 30px;
+        }
+        .main h1{
+            font-size:60px;
+            margin-top:15px;
+            color:#1CD3CB;
+            text-transform:uppercase;
+            font-family: 'Montserrat', sans-serif;
+            font-weight: 900;
+        }
+        .main p{
+            font-size:20px;
+            margin-top:15px;
+            line-height:1.6em;
+        }
+        .main  span.error > a{
+            color:#48C8D3;
+            font-size:18px;
+        }
+        .main p span{
+            font-size:14px;
+            color:#24817A;
+        }
+        .icons{
+            padding-bottom:20px;
+            text-align:right;
+        }
+        .icons p{
+            padding-right:130px;
+            color:#D5CECE;
+            font-size:13px;
+            cursor:pointer;
+        }
+        .icons p:hover{
+            text-decoration:underline;
+        }
+        .icons ul{
+            padding-right:20px;
+        }
+        .icons li {
+            display:inline-block;
+            padding-top:10px;
+        }
+        .icons li a{
+            margin:2px;
+        }
+
+        .main > p,h3 {
+            font-family: Ubuntu,sans-serif !important;
+        }
+        .main > p > span {
+            font-family: Ubuntu,sans-serif;
+        }
+        .footer {
+            visibility: visible;
+            background: rgba(255, 255, 255, 0.04);
+        }
+    </style>
+@endsection
+
 @section('contenu')
 
-    <div class="outer" style="background-color: red">
-        <div class="middle">
-            <div class="inner">
-                <div class="row text-center">
-                    <div class="big-warning">Oups! Erreur 503.</div>
-                </div>
-            </div>
+    <div class="wrap">
+        <div class="main" style="padding-bottom: 1.5em !important;">
+            <h3>LetsWork</h3>
+            <hr>
+            <!---728x90--->
+            <h1>Nous sommes en maintenance,</h1>
         </div>
     </div>
 
+@endsection
+
+@section('scriptsm')
+    <script>
+        // Media queries
+        $(document).ready(function () {
+            var sizes = function () {
+                if (window.matchMedia('(max-width: 767px)').matches) {
+                    $('.wrap')[0].style.setProperty('width','600px','important');
+                } else if (window.matchMedia('(max-width: 1060px)').matches) {
+                    $('.wrap')[0].style.setProperty('width','800px','important');
+                } else if (window.matchMedia('(max-width: 820px)').matches) {
+                    $('.wrap')[0].style.setProperty('width','600px','important');
+                } else if (window.matchMedia('(max-width: 620px)').matches) {
+                    $('.wrap')[0].style.setProperty('width','350px','important');
+                } else {
+                    $('.wrap')[0].style.setProperty('width','1000px','important');
+                }
+            };
+            sizes();
+            $(window).on('resize', function() {
+                sizes();
+            });
+        });
+    </script>
 @endsection
