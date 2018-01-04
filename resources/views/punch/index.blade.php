6b05b05b2f24ba292503f8013ac9449d86ebebfc
Unknown <charlesguertin@live.ca>
2018-01-04 16:33:53 -0500

    Punches full time fix #3

--- resources/views/punch/index.blade.php
+++ resources/views/punch/index.blade.php
@@ -50,7 +50,7 @@
                                 <td>{{\Carbon\Carbon::parse($punch->datebegin)->toDateTimeString()}}</td>
                                 @if($punch->dateend)
                                     <td>{{\Carbon\Carbon::parse($punch->dateend)->toDateTimeString()}}</td>
-                                    <td>{{\Carbon\Carbon::parse($punch->dateend)->diffInHours(\Carbon\Carbon::createFromFormat('Y-m-d H:i:s',$punch->datebegin),false)}} heure(s)</td>
+                                    <td>{{\Carbon\Carbon::parse($punch->dateend)->diffInMinutes(\Carbon\Carbon::createFromFormat('Y-m-d H:i:s',$punch->datebegin)) / 60}} heure(s)</td>
                                     {{--->diffForHumans(\Carbon\Carbon::createFromFormat('Y-m-d H:i:s',$punch->datebegin),true)--}}
                                 @else
                                     <td>Période de travail non terminée</td>
