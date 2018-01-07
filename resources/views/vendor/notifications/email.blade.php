2d65ccac55b99b7ce0558da4849ca783ac177c19
Unknown <charlesguertin@live.ca>
2018-01-07 17:24:34 -0500

    styling email

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.top')
 
 @section('styles')
+
     <style>
         .title {
             color: #000000;
@@ -10,29 +11,24 @@
             color: #000000;
         }
 
-        body {
-            background-color: white;
-        }
-
-        .footer {
-            background-color: transparent;
-        }
-
-        .footer > .text-center > span {
-            color: black;
+        p, span, a {
+            font-family: Ubuntu,sans-serif;
         }
     </style>
 @endsection
 
-@section('contenu')
-    <h1 class="title">Bonjour,</h1>
-    <div class="col-md-6">
+@section('content')
+    <h1 style="font-weight:700; font-size: 35px; color:#552ad6;font-family: Montserrat,sans-serif">LetsWork</h1>
+    <h2 class="title">Bonjour,</h2>
+    <div class="col-md-6" >
         <p class="message">
             @foreach($introLines as $lines)
                 {{ $lines }}
             @endforeach
-            <b><a href="{{ $actionUrl }}" class="link">Réinitialiser mon mot de passe.</a></b>
         </p>
+
+        <b><a style="text-decoration: none;margin-bottom: 15px;" class="btn purplebtn" href="{{ $actionUrl }}" >Réinitialiser mon mot de passe.</a></b>
+
         <p class="message">
             @foreach($outroLines as $lines)
                 {{ $lines }}
